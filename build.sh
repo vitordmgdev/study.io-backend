79e9191d7fbbc9d066a6e9821e7c723027cb76e1
vitordmgdev <123842676+vitordmgdev@users.noreply.github.com>
2024-08-04 17:32:44 -0300

    initial

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-npm run prod
-echo "Construção finalizada com sucesso!"
--- src/server.ts
+++
@@ -1,4 +0,0 @@
-import app from "./app";
-
-const PORT: number = Number(process.env.PORT) || 3000;
-app.listen(PORT, () => console.log(`Servidor ouvino na porta ${PORT}`));
